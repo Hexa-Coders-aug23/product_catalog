2356a362d5679ff04ba5c9f6260dbe9d03b2bcd8
Helen Tsyhypalo <lena.tsigipalo@gmail.com>
2023-12-11 20:37:24 +0100

    add some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import './App.module.scss';
 import React from 'react';
-// import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Footer } from './modules/shared/components/footer';
 import { Header } from './modules/shared/components/header';
-import { CatalogPage } from './modules/CatalogPage';
 
 export const App: React.FC = () => {
   return (
     <>
       <Header />
-      <CatalogPage />
+      <Outlet />
       <Footer />
     </>
   );
