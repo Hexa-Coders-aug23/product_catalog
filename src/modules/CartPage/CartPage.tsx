e45e68d46c4d4504cfeebbb748e38b340ad150b2
getsi <getsize1144@gmail.com>
2023-12-15 14:09:40 +0200

    Fixed empty cart

--- src/modules/CartPage/CartPage.tsx
+++ src/modules/CartPage/CartPage.tsx
@@ -2,10 +2,57 @@ import React from 'react';
 import styles from './CartPage.module.scss';
 
 import iconBack from '../../static/icons/Chevron_Arrow_Left.svg';
-import emptyCartImage from './empty-cart.png';
+import emptyCartImage from './1942770.png';
 // import { CartItemsList } from './CartItemsList/CartItemsList';
 
 export const CartPage: React.FC = () => {
+  // const phones = [
+  //   {
+  //     id: '1',
+  //     category: 'phones',
+  //     phoneId: 'apple-iphone-7-32gb-black',
+  //     itemId: 'apple-iphone-7-32gb-black',
+  //     name: 'Apple iPhone 7 32GB Black',
+  //     fullPrice: 400,
+  //     price: 375,
+  //     screen: "4.7' IPS",
+  //     capacity: '32GB',
+  //     color: 'black',
+  //     ram: '2GB',
+  //     year: 2016,
+  //     image: 'img/phones/apple-iphone-7/black/00.jpg',
+  //   },
+  //   {
+  //     id: '2',
+  //     category: 'phones',
+  //     phoneId: 'apple-iphone-7-plus-32gb-black',
+  //     itemId: 'apple-iphone-7-plus-32gb-black',
+  //     name: 'Apple iPhone 7 Plus 32GB Black',
+  //     fullPrice: 540,
+  //     price: 500,
+  //     screen: "5.5' IPS",
+  //     capacity: '32GB',
+  //     color: 'black',
+  //     ram: '3GB',
+  //     year: 2016,
+  //     image: 'img/phones/apple-iphone-7-plus/black/00.jpg',
+  //   },
+  //   {
+  //     id: '3',
+  //     category: 'phones',
+  //     phoneId: 'apple-iphone-8-64gb-gold',
+  //     itemId: 'apple-iphone-8-64gb-gold',
+  //     name: 'Apple iPhone 8 64GB Gold',
+  //     fullPrice: 600,
+  //     price: 550,
+  //     screen: "4.7' IPS",
+  //     capacity: '64GB',
+  //     color: 'gold',
+  //     ram: '2GB',
+  //     year: 2017,
+  //     image: 'img/phones/apple-iphone-8/gold/00.jpg',
+  //   }];
+
   return (
     <main className={styles.container}>
       <button type="button" className={styles.goBackButton}>
@@ -23,7 +70,7 @@ export const CartPage: React.FC = () => {
       {/* {!phones.length ? ( */}
       <div className={styles.emptyCart}>
         <img
-          className={styles.emptyCart}
+          className={styles.emptyCartImage}
           src={emptyCartImage}
           alt="Empty Cart"
         />
@@ -38,10 +85,10 @@ export const CartPage: React.FC = () => {
         </p>
       </div>
       {/* ) : ( */}
-      <div className={styles.cartContent}>
-        {/* <CartItemsList phones={phones} /> */}
+      {/* <div className={styles.cartContent}>
+        <CartItemsList phones={phones} />
 
-        {/* <div className={styles.checkout}>
+        <div className={styles.checkout}>
           <h2 className={styles.checkoutPrice}>$2997</h2>
 
           <p className={styles.checkoutAmountItems}>Total for 3 items</p>
@@ -51,8 +98,8 @@ export const CartPage: React.FC = () => {
           <button className={styles.checkoutButton} type="button">
             Checkout
           </button>
-        </div> */}
-      </div>
+        </div>
+      </div> */}
       {/* )} */}
     </main>
   );
