3b438439180f6b3bbb3e335d640e28f20722ce32
Dmytro Panin <dmitriyp1598@gmail.com>
2023-12-20 15:48:40 +0200

    fixed not gound page

--- src/modules/NotFoundPage/NotFoundPage.tsx
+++ src/modules/NotFoundPage/NotFoundPage.tsx
@@ -2,25 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './NotFoundPage.module.scss';
 
-import errorImage from '../../static/error/error_image.jpeg';
+import errorImage from '../../static/error/robot_error.png';
 
 export const NotFoundPage: React.FC = () => {
   return (
-    <main className={styles.error}>
-      <h1 className={styles.error_type}>Page not found</h1>
-      <p className={styles.error_discription}>
-        Sorry, we couldn&apos;t find the page you&apos;re looking for. Perhabs
-        you&apos;ve mistyped the URL?
-      </p>
-
-      <p className={styles.error_suggestion}>
-        <p>Be sure to check your spelling or</p>
-        <Link to="/home" className={styles.error_fix}>
-          &nbsp;go to home page.
-        </Link>
-      </p>
-
+    <main className={`${styles.error} ${styles.container}`}>
       <img src={errorImage} alt="not found" className={styles.error_image} />
+
+      <div className={styles.error_suggestion}>
+        <h1 className={styles.error_type}>page not found</h1>
+        <p className={styles.error_discription}>
+          We Couldn&apos;t Find The Page You Were Looking For
+        </p>
+
+        <Link to="/home" className={styles.error_fix}>
+          <p className={styles.error_fix_text}>Back To Home</p>
+        </Link>
+      </div>
     </main>
   );
 };
